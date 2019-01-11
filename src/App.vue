ca436a9981dc49010bbc1647b4eae6d3f20e6781
Sastha <shazcool@gmail.com>
2019-01-11 10:05:43 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,89 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div>
+    <ag-grid-vue style="width: 500px; height: 500px;"
+                   class="ag-theme-balham"
+                   :columnDefs="columnDefs"
+                   :rowData="rowData"
+                   :cellValueChanged="cellValueChanged"
+                   :gridReady="onGridReady"
+                   :gridOptions="gridOptions">
+      </ag-grid-vue>
+      <button @click="changeColor">Change Color</button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {AgGridVue} from "ag-grid-vue";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        data() {
+            return {
+                columnDefs: null,
+                rowData: null,
+                gridApi: null,
+                columnApi: null,
+                gridOptions: null
+            }
+        },
+        components: {
+            AgGridVue
+        },
+        methods: {
+          onGridReady(params) {
+                          this.gridApi = params.api;
+                          this.columnApi = params.columnApi;
+                          console.log(params);
+                      },
+          cellValueChanged: function(param) {
+            console.log(param);
+          },
+          changeColor: function() {
+            var rowNode = this.gridOptions.api.getRowNode(0);
+            rowNode.setDataValue('model', 'Toyota');
+          }
+        },
+
+        beforeMount() {
+            this.gridOptions = {};
+            this.columnDefs = [
+                {headerName: 'Make', field: 'make'},
+                {headerName: 'Model', field: 'model',editable:true,
+                  cellClassRules: {
+                              'rag-green': 'x == "Mondeo"',
+                              'rag-red': 'x == "Celica"',
+                          }
+                },
+                {headerName: 'Price', field: 'price'}
+            ];
+
+            this.rowData = [
+                {id: 1, make: 'Toyota', model: 'Celica', price: 35000},
+                {id: 2, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 3, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 4, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 5, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 6, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 7, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 8, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 9, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 10, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 11, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 12, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 13, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 14, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 15, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 16, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 17, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 18, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 19, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 20, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 21, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 22, make: 'Ford', model: 'Mondeo', price: 32000},
+                {id: 23, make: 'Porsche', model: 'Boxter', price: 72000}
+            ];
+        }
+    }
 </script>
 
 <style>
@@ -24,5 +94,26 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.rag-red {
+    background-color: lightcoral;
+}
+.rag-green {
+    background-color: lightgreen;
+}
+.rag-amber {
+    background-color: lightsalmon;
+}
+
+.rag-red-outer .rag-element {
+    background-color: lightcoral;
+}
+
+.rag-green-outer .rag-element {
+    background-color: lightgreen;
+}
+
+.rag-amber-outer .rag-element {
+    background-color: lightsalmon;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import "../node_modules/ag-grid-community/dist/styles/ag-grid.css";
+import "../node_modules/ag-grid-community/dist/styles/ag-theme-balham.css";
+
 Vue.config.productionTip = false
 
 new Vue({
